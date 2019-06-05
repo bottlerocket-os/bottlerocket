954127ced40dddd971383ab36ec54bec5bc84d94
iliana destroyer of worlds <iweller@amazon.com>
2019-06-05 21:26:57 +0000

    signpost: Improve clarity of State::load
    Signed-off-by: iliana destroyer of worlds <iweller@amazon.com>

--- workspaces/signpost/src/state/mod.rs
+++ workspaces/signpost/src/state/mod.rs
@@ -20,12 +20,26 @@ const THAR_HASH: [u8; 16] = uuid_to_guid(hex!("598f10af c955 4456 6a99 7720068a6
 pub(crate) struct State {
     os_disk: PathBuf,
     sets: [PartitionSet; 2],
-    boot_nums: [u32; 2],
+    /// The partition numbers that correspond to the boot partitions in each partition set,
+    /// respectively.
+    ///
+    /// This is used to load the correct partition flags from `table`.
+    boot_partition_nums: [u32; 2],
     table: GPT,
     active: SetSelect,
 }
 
 impl State {
+    /// Loads the state of the universe:
+    ///
+    /// * Finds the device corresponding to the root filesystem mount (`/`), which is assumed to be
+    ///   a dm-verity device.
+    /// * Gets the first lower device, which will either be the root or hash partition of the
+    ///   active partition set.
+    /// * Find the first and second partitions matching each of the boot, root, and hash partition
+    ///   type GUIDs. The first partitions are set A and the second partitions are set B.
+    /// * Determine which partition set is active by finding which one contains the partition we
+    ///   found from our root filesystem earlier.
     pub(crate) fn load() -> Result<Self, Error> {
         // The root filesystem is a dm-verity device. We want to determine what disk and partition
         // the backing data is part of. Look up the device major and minor via stat(2):
@@ -41,7 +55,7 @@ impl State {
         let os_disk = active_partition.disk()?;
         let active_partition = active_partition.path();
 
-        // Parse the partition table on the disk and detect the partition sets.
+        // Parse the partition table on the disk.
         let table = GPT::find_from(&mut File::open(os_disk.path()).context(error::Open {
             path: os_disk.path(),
             what: "reading",
@@ -51,6 +65,7 @@ impl State {
             device: os_disk.path(),
         })?;
 
+        // Finds the nth partition on `table` matching the partition type GUID `guid`.
         let nth_guid = |guid, n| -> Result<u32, Error> {
             Ok(table
                 .iter()
@@ -62,7 +77,8 @@ impl State {
                 })?
                 .0)
         };
-        let disk = |num| -> Result<PathBuf, Error> {
+        // Loads the path to partition number `num` on the OS disk.
+        let device_from_part_num = |num| -> Result<PathBuf, Error> {
             Ok(os_disk
                 .partition(num)?
                 .context(error::PartitionNotFoundOnDevice {
@@ -72,20 +88,22 @@ impl State {
                 .path())
         };
 
-        let boot_nums = [nth_guid(THAR_BOOT, 0)?, nth_guid(THAR_BOOT, 1)?];
+        let boot_partition_nums = [nth_guid(THAR_BOOT, 0)?, nth_guid(THAR_BOOT, 1)?];
         let sets = [
             PartitionSet {
-                boot: disk(boot_nums[0])?,
-                root: disk(nth_guid(THAR_ROOT, 0)?)?,
-                hash: disk(nth_guid(THAR_HASH, 0)?)?,
+                boot: device_from_part_num(boot_partition_nums[0])?,
+                root: device_from_part_num(nth_guid(THAR_ROOT, 0)?)?,
+                hash: device_from_part_num(nth_guid(THAR_HASH, 0)?)?,
             },
             PartitionSet {
-                boot: disk(boot_nums[1])?,
-                root: disk(nth_guid(THAR_ROOT, 1)?)?,
-                hash: disk(nth_guid(THAR_HASH, 1)?)?,
+                boot: device_from_part_num(boot_partition_nums[1])?,
+                root: device_from_part_num(nth_guid(THAR_ROOT, 1)?)?,
+                hash: device_from_part_num(nth_guid(THAR_HASH, 1)?)?,
             },
         ];
 
+        // Determine which set is active by seeing which set contains the current running root or
+        // hash partition.
         let active = if sets[0].contains(&active_partition) {
             SetSelect::A
         } else if sets[1].contains(&active_partition) {
@@ -101,7 +119,7 @@ impl State {
         Ok(Self {
             os_disk: os_disk.path(),
             sets,
-            boot_nums,
+            boot_partition_nums,
             table,
             active,
         })
@@ -116,11 +134,11 @@ impl State {
     }
 
     fn gptprio(&self, select: SetSelect) -> GptPrio {
-        GptPrio::from(self.table[self.boot_nums[select.idx()]].attribute_bits)
+        GptPrio::from(self.table[self.boot_partition_nums[select.idx()]].attribute_bits)
     }
 
     fn set_gptprio(&mut self, select: SetSelect, flags: GptPrio) {
-        self.table[self.boot_nums[select.idx()]].attribute_bits = flags.into();
+        self.table[self.boot_partition_nums[select.idx()]].attribute_bits = flags.into();
     }
 
     pub(crate) fn active(&self) -> SetSelect {
