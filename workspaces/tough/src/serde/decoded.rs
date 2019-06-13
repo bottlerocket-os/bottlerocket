271bd13d2c8d3b813371dc1df552ef06d8095ceb
iliana destroyer of worlds <iweller@amazon.com>
2019-06-13 22:53:17 +0000

    tough: Gently rearrange crate::serde::decoded
    Signed-off-by: iliana destroyer of worlds <iweller@amazon.com>

--- workspaces/tough/src/serde/decoded.rs
+++ workspaces/tough/src/serde/decoded.rs
@@ -1,70 +1,68 @@
 use serde::{de::Error, Deserialize, Deserializer, Serialize, Serializer};
 use std::cmp::Ordering;
-use std::fmt::{self, Debug, Display};
+use std::fmt::{self, Debug};
 use std::marker::PhantomData;
 use std::ops::Deref;
 
-pub(crate) struct Decoded<T> {
+/// Represents bytes decoded from a string.
+///
+/// The type parameter `T` represents what kind of data the original string stores (e.g.
+/// hex-encoded bytes, or a PEM-encoded key).
+///
+/// The original string is stored so that it can be re-`Serialize`d for the purposes of verifying
+/// signatures.
+pub(crate) struct Decoded<T: Decode> {
     bytes: Vec<u8>,
     original: String,
     spooky: PhantomData<T>,
 }
 
-impl<T> Decoded<T> {
+impl<T: Decode> Decoded<T> {
+    /// Consume this object and return its bytes.
     pub(crate) fn into_vec(self) -> Vec<u8> {
         self.bytes
     }
 }
 
-impl<T> Deref for Decoded<T> {
-    type Target = [u8];
+// =^..^=   =^..^=   =^..^=   =^..^=   =^..^=   =^..^=   =^..^=
 
-    fn deref(&self) -> &[u8] {
-        &self.bytes
+/// A type that represents how data can be converted from a string to bytes.
+///
+/// Generally structs that implement `Decode` will be unit-like structs that just implement the one
+/// required method.
+pub(crate) trait Decode {
+    fn parse<'de, D>(s: &str) -> Result<Vec<u8>, D::Error>
+    where
+        D: Deserializer<'de>;
+}
+
+/// [`Decode`] implementation for hex-encoded strings.
+pub(crate) struct Hex;
+
+impl Decode for Hex {
+    fn parse<'de, D>(s: &str) -> Result<Vec<u8>, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        hex::decode(s).map_err(|err| D::Error::custom(format!("invalid hex string: {}", err)))
     }
 }
 
-impl<T> Debug for Decoded<T> {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        Debug::fmt(&self.original, f)
+/// [`Decode`] implementation for PEM-encoded keys.
+pub(crate) struct Pem;
+
+impl Decode for Pem {
+    fn parse<'de, D>(s: &str) -> Result<Vec<u8>, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        pem::parse(s)
+            .map(|pem| pem.contents)
+            .map_err(|err| D::Error::custom(format!("invalid PEM string: {}", err)))
     }
 }
 
-impl<T> Display for Decoded<T> {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        Display::fmt(&self.original, f)
-    }
-}
-
-impl<T> Clone for Decoded<T> {
-    fn clone(&self) -> Self {
-        Self {
-            bytes: self.bytes.clone(),
-            original: self.original.clone(),
-            spooky: PhantomData,
-        }
-    }
-}
-
-impl<T> PartialEq for Decoded<T> {
-    fn eq(&self, other: &Self) -> bool {
-        self.bytes.eq(&other.bytes)
-    }
-}
-
-impl<T> Eq for Decoded<T> {}
-
-impl<T> PartialOrd for Decoded<T> {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        self.bytes.partial_cmp(&other.bytes)
-    }
-}
-
-impl<T> Ord for Decoded<T> {
-    fn cmp(&self, other: &Self) -> Ordering {
-        self.bytes.cmp(&other.bytes)
-    }
-}
+// =^..^=   =^..^=   =^..^=   =^..^=   =^..^=   =^..^=   =^..^=
 
 impl<'de, T: Decode> Deserialize<'de> for Decoded<T> {
     fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
@@ -89,32 +87,48 @@ impl<T: Decode> Serialize for Decoded<T> {
     }
 }
 
-pub(crate) trait Decode: Sized {
-    fn parse<'de, D>(s: &str) -> Result<Vec<u8>, D::Error>
-    where
-        D: Deserializer<'de>;
-}
+// =^..^=   =^..^=   =^..^=   =^..^=   =^..^=   =^..^=   =^..^=
 
-pub(crate) struct Hex;
+impl<T: Decode> Deref for Decoded<T> {
+    type Target = [u8];
 
-impl Decode for Hex {
-    fn parse<'de, D>(s: &str) -> Result<Vec<u8>, D::Error>
-    where
-        D: Deserializer<'de>,
-    {
-        hex::decode(s).map_err(|err| D::Error::custom(format!("invalid hex string: {}", err)))
+    fn deref(&self) -> &[u8] {
+        &self.bytes
     }
 }
 
-pub(crate) struct Pem;
+impl<T: Decode> Debug for Decoded<T> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        Debug::fmt(&self.original, f)
+    }
+}
 
-impl Decode for Pem {
-    fn parse<'de, D>(s: &str) -> Result<Vec<u8>, D::Error>
-    where
-        D: Deserializer<'de>,
-    {
-        pem::parse(s)
-            .map(|pem| pem.contents)
-            .map_err(|err| D::Error::custom(format!("invalid PEM string: {}", err)))
+impl<T: Decode> Clone for Decoded<T> {
+    fn clone(&self) -> Self {
+        Self {
+            bytes: self.bytes.clone(),
+            original: self.original.clone(),
+            spooky: PhantomData,
+        }
+    }
+}
+
+impl<T: Decode> PartialEq for Decoded<T> {
+    fn eq(&self, other: &Self) -> bool {
+        self.bytes.eq(&other.bytes)
+    }
+}
+
+impl<T: Decode> Eq for Decoded<T> {}
+
+impl<T: Decode> PartialOrd for Decoded<T> {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        self.bytes.partial_cmp(&other.bytes)
+    }
+}
+
+impl<T: Decode> Ord for Decoded<T> {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.bytes.cmp(&other.bytes)
     }
 }
