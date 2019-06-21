28488ae7031cd5007f5fa1f53b6d954fc57e65b8
Jacob Vallejo <jakeev@amazon.com>
2019-06-20 22:08:22 -0700

    nix: tidy up some whitespace

--- nix/default.nix
+++ nix/default.nix
@@ -4,18 +4,18 @@ let
   nixpkgs' = tharNixpkgs {}; # configure nixpkgs for use (upstream config).
 
   overlaidPkgs = tharNixpkgs { overlays = [ (self: super: tharClosure self) ]; };
-  
+
   tharClosure = self: let
     # thar closure's callPackage for resolving attributes from the
     # final set of attributes in `packages'.
     callPackage = (self.newScope packages);
-    
+
     buildSupport = rec {
       # External dependencies can be pulled in using `pkgs'.
       pkgs = self;
       lib = pkgs.lib;
       overlaid = overlaidPkgs;
-      
+
       # Explicit inclusion of common external dependency in scope, others
       # are provided directly as needed.
       inherit (pkgs)
@@ -24,10 +24,14 @@ let
         # be folk's default.
         stdenvNoCC symlinkJoin writeScript;
 
+
+      # Provide config and the scope's callPackage for nested
+      # derivations.
       inherit config callPackage;
 
+      # TODO: this attribute should be based on the architecture being
+      # targeted.
       inherit (config) base-container-image;
-
 
       # Docker CLI is the Nix pure docker derivation, but only the
       # docker cli to avoid having to copy down the entirety of the
@@ -43,7 +47,7 @@ let
         teardown = pkgs.callPackage ./docker/container-teardown.nix {};
       };
       docker-sanity = callPackage ./docker/sanity-check.nix {};
-      
+
       rpm-metadata = callPackage ./rpm/rpm-metadata.nix { inherit (pkgs) rpm; };
       rpm-macros = callPackage ./rpm/rpm-macros.nix { inherit (pkgs) rpm; };
       rpm-container = callPackage ./rpm/rpm-container.nix { };
@@ -53,11 +57,9 @@ let
 
       example = callPackage ./example/default.nix {};
     };
-
     buildPackages =  import ../packages { inherit callPackage; };
-
     packages = buildSupport // buildPackages;
-    
+
   in packages;
 in
 tharClosure nixpkgs'
