a19f52163a462fdec509a209c9416f656d5334c3
Jacob Vallejo <jakeev@amazon.com>
2019-06-20 20:19:00 -0700

    nix: explicitly pull images prior to build

--- nix/docker/docker-image.nix
+++ nix/docker/docker-image.nix
@@ -20,6 +20,7 @@ let
       ref="''${containerRef##*/}"
       ref="''${ref,,}:containerRef"
 
+      awk '/FROM/ { $1=""; print; }' ${dockerfileFile} | xargs --no-run-if-empty -L1 -t docker pull
       docker build --build-arg name \
                    --build-arg containerRef \
                    --label containerRef=$containerRef \
