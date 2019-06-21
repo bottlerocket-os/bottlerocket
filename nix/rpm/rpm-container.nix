07e2f5bef09671a29a26d22c1ccf7e646c028f1c
Jacob Vallejo <jakeev@amazon.com>
2019-06-20 17:15:35 -0700

    nix: separate build needs into layers, revisit needed
    This should *not* be how this is performed, the container's updates
    should be canonicalized and made to be as _pure_ as possible to avoid
    having a changing environment and perhaps result as an effect.

--- nix/rpm/rpm-container.nix
+++ nix/rpm/rpm-container.nix
@@ -1,14 +1,21 @@
-{ docker-image, base-container-image }:
+{ lib, docker-image, base-container-image }:
 let
   baseImage = base-container-image.ref;
+
+  # Dependencies for the base image and building
+  essentialDeps = lib.escapeShellArgs [ "rpmdevtools" "dnf-plugins-core" "createrepo_c" ];
+  # Dependencies needed for the bootstrapping package build.
+  bootstrapDeps = lib.escapeShellArgs [ "wget" "python" "perl-ExtUtils-MakeMaker" "bc" "rsync" ];
 in
 # TODO: make this more pure, where possible.
 docker-image.mkImage {
   name = "rpm-build-container-image";
+
   dockerfile = ''
   FROM ${baseImage}
-  RUN dnf upgrade -y && \
-      dnf groupinstall -y "C Development Tools and Libraries" && \
-      dnf install -y rpmdevtools dnf-plugins-core createrepo_c
+  RUN dnf upgrade -y
+  RUN dnf groupinstall -y "C Development Tools and Libraries"
+  RUN dnf install -y ${essentialDeps}
+  RUN dnf install -y ${bootstrapDeps}
   '';
 }
