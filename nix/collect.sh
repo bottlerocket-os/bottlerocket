a580579f9fb33c37a6fd4248c3b7c4e77efb9e07
Jacob Vallejo <jakeev@amazon.com>
2019-06-24 12:27:46 -0700

    nix: update hack for updating package list

--- nix/collect.sh
+++ nix/collect.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-package_listing=$(printf "  %s = callPackage ./%s {};\n" \
+package_listing=$(printf "    %s = callPackage ./%s {};\n" \
 	$(find ./packages/ -mindepth 2 -name default.nix \
 	      | sed 's,./packages/,,' \
               | awk -F'/' '{ print $(NF-1), $0 }'))
@@ -8,7 +8,16 @@ package_listing=$(printf "  %s = callPackage ./%s {};\n" \
 
 cat <<EOF
 { callPackage }:
-{
-$package_listing  
-}
+let
+  tharPackages = rec {
+    inherit tharPackages;
+
+$package_listing
+
+    # Aliases
+    gcc = sdk;
+    kernel-headers = kernel;
+  };
+in
+tharPackages
 EOF
--- packages/default.nix
+++ packages/default.nix
@@ -3,21 +3,32 @@ let
   tharPackages = rec {
     inherit tharPackages;
 
-    sdk = callPackage ./sdk {};
+    signpost = callPackage ./signpost/default.nix {};
+    bash = callPackage ./bash/default.nix {};
+    coreutils = callPackage ./coreutils/default.nix {};
+    sdk = callPackage ./sdk/default.nix {};
+    readline = callPackage ./readline/default.nix {};
+    ncurses = callPackage ./ncurses/default.nix {};
+    libxcrypt = callPackage ./libxcrypt/default.nix {};
+    rust = callPackage ./rust/default.nix {};
+    api = callPackage ./api/default.nix {};
+    libacl = callPackage ./libacl/default.nix {};
+    libattr = callPackage ./libattr/default.nix {};
+    util-linux = callPackage ./util-linux/default.nix {};
+    systemd = callPackage ./systemd/default.nix {};
+    release = callPackage ./release/default.nix {};
+    libkmod = callPackage ./libkmod/default.nix {};
+    grub = callPackage ./grub/default.nix {};
+    filesystem = callPackage ./filesystem/default.nix {};
+    libcap = callPackage ./libcap/default.nix {};
+    glibc = callPackage ./glibc/default.nix {};
+    strace = callPackage ./strace/default.nix {};
+    kernel = callPackage ./kernel/default.nix {};
+    ripgrep = callPackage ./ripgrep/default.nix {};
+
+    # Aliases
     gcc = sdk;
-
-    glibc = callPackage ./glibc {};
-
-    kernel = callPackage ./kernel {};
     kernel-headers = kernel;
-
-    ncurses = callPackage ./ncurses {};
-    readline = callPackage ./readline {};
-    libattr = callPackage ./libattr {};
-    libacl = callPackage ./libacl {};
-    bash = callPackage ./bash {};
-    signpost = callPackage ./signpost {};
-    rust = callPackage ./rust {};
   };
 in
 tharPackages
