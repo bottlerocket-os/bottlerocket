0b881600760a3799592fd7e948c11444b1201488
iliana destroyer of worlds <iweller@amazon.com>
2019-09-05 21:41:21 +0000

    tuftool: Ensure clobbering files in outdir works
    Signed-off-by: iliana destroyer of worlds <iweller@amazon.com>

--- workspaces/tuftool/src/copylike.rs
+++ workspaces/tuftool/src/copylike.rs
@@ -1,5 +1,6 @@
 use std::fmt::{self, Display};
-use std::fs::{self};
+use std::fs;
+use std::io;
 use std::path::Path;
 
 #[derive(Debug, Clone, Copy)]
@@ -10,10 +11,18 @@ pub(crate) enum Copylike {
 }
 
 impl Copylike {
-    pub(crate) fn run<P: AsRef<Path>, Q: AsRef<Path>>(self, src: P, dst: Q) -> std::io::Result<()> {
+    pub(crate) fn run<P: AsRef<Path>, Q: AsRef<Path>>(self, src: P, dst: Q) -> io::Result<()> {
         if let Some(parent) = dst.as_ref().parent() {
             fs::create_dir_all(parent)?;
         }
+
+        // unlinking the file is required before symlink/hardlink
+        if let Err(err) = fs::remove_file(&dst) {
+            if err.kind() != io::ErrorKind::NotFound {
+                return Err(err);
+            }
+        }
+
         match self {
             Copylike::Copy => fs::copy(src, dst).map(|_| ()),
             Copylike::Hardlink => fs::hard_link(src, dst),
@@ -43,5 +52,36 @@ impl Display for Copylike {
                 Copylike::Symlink => "symlink",
             }
         )
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Copylike;
+    use std::fs;
+    use std::io;
+
+    #[test]
+    fn clobber() -> io::Result<()> {
+        let dir = tempfile::tempdir()?;
+        let a = dir.path().join("a");
+        let b = dir.path().join("b");
+
+        {
+            fs::File::create(&a)?;
+            fs::File::create(&b)?;
+        }
+
+        for copy_action in &[Copylike::Copy, Copylike::Hardlink, Copylike::Symlink] {
+            eprintln!("{:?}", copy_action);
+            let target = dir.path().join(copy_action.to_string());
+            copy_action.run(&a, &target)?;
+            // copying the same file should work
+            copy_action.run(&a, &target)?;
+            // copying a different file should clobber
+            copy_action.run(&b, &target)?;
+        }
+
+        Ok(())
     }
 }
