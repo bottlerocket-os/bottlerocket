7cf5320bc2470ca3b1e7553f0ca46a95601a1096
iliana destroyer of worlds <iweller@amazon.com>
2019-09-12 20:53:24 +0000

    tuftool: Add `root expire` subcommand
    Signed-off-by: iliana destroyer of worlds <iweller@amazon.com>

--- workspaces/tuftool/src/root.rs
+++ workspaces/tuftool/src/root.rs
@@ -1,9 +1,10 @@
 use crate::error::{self, Result};
-use chrono::Utc;
+use chrono::{DateTime, Timelike, Utc};
 use maplit::hashmap;
 use serde::Serialize;
 use snafu::{OptionExt, ResultExt};
 use std::collections::HashMap;
+use std::fs::File;
 use std::io::Write;
 use std::num::NonZeroU64;
 use std::path::{Path, PathBuf};
@@ -15,8 +16,15 @@ use tough_schema::{RoleKeys, RoleType, Root, Signed};
 pub(crate) enum Command {
     /// Create a new root.json metadata file
     Init {
-        /// Path where root.json is written
+        /// Path to new root.json
         path: PathBuf,
+    },
+    /// Set the expiration time for root.json
+    Expire {
+        /// Path to root.json
+        path: PathBuf,
+        /// When to expire
+        time: DateTime<Utc>,
     },
 }
 
@@ -24,14 +32,14 @@ macro_rules! role_keys {
     ($threshold:expr) => {
         RoleKeys {
             keyids: Vec::new(),
-            threshold: NonZeroU64::new($threshold).unwrap(),
+            threshold: $threshold,
             _extra: HashMap::new(),
         }
     };
 
     () => {
         // absurdly high threshold value so that someone realizes they need to change this
-        role_keys!(1507)
+        role_keys!(NonZeroU64::new(1507).unwrap())
     };
 }
 
@@ -45,7 +53,7 @@ impl Command {
                         spec_version: "1.0".to_owned(),
                         consistent_snapshot: true,
                         version: NonZeroU64::new(1).unwrap(),
-                        expires: Utc::now(),
+                        expires: round_time(Utc::now()),
                         keys: HashMap::new(),
                         roles: hashmap! {
                             RoleType::Root => role_keys!(),
@@ -58,8 +66,23 @@ impl Command {
                     signatures: Vec::new(),
                 },
             ),
+            Command::Expire { path, time } => {
+                let mut root = load_root(path)?;
+                root.signed.expires = round_time(*time);
+                write_json(path, &root)
+            }
         }
     }
+}
+
+fn round_time(time: DateTime<Utc>) -> DateTime<Utc> {
+    // `Timelike::with_nanosecond` returns None only when passed a value >= 2_000_000_000
+    time.with_nanosecond(0).unwrap()
+}
+
+fn load_root(path: &Path) -> Result<Signed<Root>> {
+    serde_json::from_reader(File::open(path).context(error::FileOpen { path })?)
+        .context(error::FileParseJson { path })
 }
 
 fn write_json<T: Serialize>(path: &Path, json: &T) -> Result<()> {
