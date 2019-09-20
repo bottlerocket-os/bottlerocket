7df20835be35d856cad276fb00a5997ecfa89c33
iliana destroyer of worlds <iweller@amazon.com>
2019-09-20 05:26:54 +0000

    tough_schema: Add `_extra` fields to Key structs
    All structs that are deserialized as part of signed content need to have
    extra fields stored for signature verification. I missed the structs in
    `key.rs` on my initial pass a while back.
    
    Signed-off-by: iliana destroyer of worlds <iweller@amazon.com>

--- workspaces/updater/tough_schema/src/key.rs
+++ workspaces/updater/tough_schema/src/key.rs
@@ -3,60 +3,80 @@ use crate::error::{self, Result};
 use olpc_cjson::CanonicalFormatter;
 use ring::signature::VerificationAlgorithm;
 use serde::{Deserialize, Serialize};
+use serde_json::Value;
 use sha2::{Digest, Sha256};
 use snafu::ResultExt;
+use std::collections::HashMap;
 use std::fmt;
 use std::str::FromStr;
 
-#[derive(Debug, Clone, Deserialize, Serialize, PartialEq, Eq)]
+#[derive(Debug, Clone, Deserialize, Serialize, PartialEq)]
 #[serde(rename_all = "kebab-case")]
 #[serde(tag = "keytype")]
 pub enum Key {
     Rsa {
         keyval: RsaKey,
         scheme: RsaScheme,
+
+        #[serde(flatten)]
+        _extra: HashMap<String, Value>,
     },
     Ed25519 {
         keyval: Ed25519Key,
         scheme: Ed25519Scheme,
+
+        #[serde(flatten)]
+        _extra: HashMap<String, Value>,
     },
     Ecdsa {
         keyval: EcdsaKey,
         scheme: EcdsaScheme,
+
+        #[serde(flatten)]
+        _extra: HashMap<String, Value>,
     },
 }
 
-#[derive(Debug, Clone, Deserialize, Serialize, PartialEq, Eq)]
+#[derive(Debug, Clone, Deserialize, Serialize, PartialEq)]
 #[serde(rename_all = "kebab-case")]
 pub enum RsaScheme {
     RsassaPssSha256,
 }
 
-#[derive(Debug, Clone, Deserialize, Serialize, PartialEq, Eq)]
+#[derive(Debug, Clone, Deserialize, Serialize, PartialEq)]
 pub struct RsaKey {
     pub public: Decoded<RsaPem>,
+
+    #[serde(flatten)]
+    pub _extra: HashMap<String, Value>,
 }
 
-#[derive(Debug, Clone, Deserialize, Serialize, PartialEq, Eq)]
+#[derive(Debug, Clone, Deserialize, Serialize, PartialEq)]
 #[serde(rename_all = "kebab-case")]
 pub enum Ed25519Scheme {
     Ed25519,
 }
 
-#[derive(Debug, Clone, Deserialize, Serialize, PartialEq, Eq)]
+#[derive(Debug, Clone, Deserialize, Serialize, PartialEq)]
 pub struct Ed25519Key {
     pub public: Decoded<Hex>,
+
+    #[serde(flatten)]
+    pub _extra: HashMap<String, Value>,
 }
 
-#[derive(Debug, Clone, Deserialize, Serialize, PartialEq, Eq)]
+#[derive(Debug, Clone, Deserialize, Serialize, PartialEq)]
 #[serde(rename_all = "kebab-case")]
 pub enum EcdsaScheme {
     EcdsaSha2Nistp256,
 }
 
-#[derive(Debug, Clone, Deserialize, Serialize, PartialEq, Eq)]
+#[derive(Debug, Clone, Deserialize, Serialize, PartialEq)]
 pub struct EcdsaKey {
     pub public: Decoded<EcdsaPem>,
+
+    #[serde(flatten)]
+    pub _extra: HashMap<String, Value>,
 }
 
 impl Key {
@@ -76,6 +96,7 @@ impl Key {
             Key::Ecdsa {
                 scheme: EcdsaScheme::EcdsaSha2Nistp256,
                 keyval,
+                ..
             } => (
                 &ring::signature::ECDSA_P256_SHA256_ASN1,
                 untrusted::Input::from(&keyval.public),
@@ -83,6 +104,7 @@ impl Key {
             Key::Ed25519 {
                 scheme: Ed25519Scheme::Ed25519,
                 keyval,
+                ..
             } => (
                 &ring::signature::ED25519,
                 untrusted::Input::from(&keyval.public),
@@ -90,6 +112,7 @@ impl Key {
             Key::Rsa {
                 scheme: RsaScheme::RsassaPssSha256,
                 keyval,
+                ..
             } => (
                 &ring::signature::RSA_PSS_2048_8192_SHA256,
                 untrusted::Input::from(&keyval.public),
@@ -111,22 +134,34 @@ impl FromStr for Key {
     fn from_str(s: &str) -> std::result::Result<Self, Self::Err> {
         if let Ok(public) = serde_plain::from_str::<Decoded<RsaPem>>(s) {
             Ok(Key::Rsa {
-                keyval: RsaKey { public },
+                keyval: RsaKey {
+                    public,
+                    _extra: HashMap::new(),
+                },
                 scheme: RsaScheme::RsassaPssSha256,
+                _extra: HashMap::new(),
             })
         } else if let Ok(public) = serde_plain::from_str::<Decoded<Hex>>(s) {
             if public.len() == ring::signature::ED25519_PUBLIC_KEY_LEN {
                 Ok(Key::Ed25519 {
-                    keyval: Ed25519Key { public },
+                    keyval: Ed25519Key {
+                        public,
+                        _extra: HashMap::new(),
+                    },
                     scheme: Ed25519Scheme::Ed25519,
+                    _extra: HashMap::new(),
                 })
             } else {
                 Err(KeyParseError(()))
             }
         } else if let Ok(public) = serde_plain::from_str::<Decoded<EcdsaPem>>(s) {
             Ok(Key::Ecdsa {
-                keyval: EcdsaKey { public },
+                keyval: EcdsaKey {
+                    public,
+                    _extra: HashMap::new(),
+                },
                 scheme: EcdsaScheme::EcdsaSha2Nistp256,
+                _extra: HashMap::new(),
             })
         } else {
             Err(KeyParseError(()))
