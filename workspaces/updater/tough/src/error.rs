b8d134b1978b708fcf80aefacb4107a5ba03cab2
iliana destroyer of worlds <iweller@amazon.com>
2019-09-20 05:41:18 +0000

    tough: Handle file:/// URLs
    Signed-off-by: iliana destroyer of worlds <iweller@amazon.com>

--- workspaces/updater/tough/src/error.rs
+++ workspaces/updater/tough/src/error.rs
@@ -45,6 +45,14 @@ pub enum Error {
         backtrace: Backtrace,
     },
 
+    /// A file from a `file:///` URL could not be opened.
+    #[snafu(display("Failed to read {}: {}", path.display(), source))]
+    FileRead {
+        path: PathBuf,
+        source: std::io::Error,
+        backtrace: Backtrace,
+    },
+
     /// A downloaded target's checksum does not match the checksum listed in the repository
     /// metadata.
     #[snafu(display(
