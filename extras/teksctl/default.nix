48cbe47e6c339444f72936b597a70376dd691999
Jacob Vallejo <jakeev@amazon.com>
2019-10-03 14:50:57 -0700

    tekctl: update to 0.1.4

--- extras/teksctl/default.nix
+++ extras/teksctl/default.nix
@@ -1,9 +1,12 @@
 { lib, buildGoModule, fetchFromGitHub }:
+{ amiID, versionExtra ? "" }:
 let
   rev = "0bf29e753d6c90a04fe0f8255d345fde3684fdf2";
   snapshotDate = "20191002";
 in
 buildGoModule rec {
+  inherit amiID;
+  
   pname = "eksctl";
   version = "0.6.0";
 
@@ -16,16 +19,27 @@ buildGoModule rec {
   modSha256 = "19my0xfssgki18syqfwbd2n8iasajy4zg0jblb0pg35lh145zz3a";
   subPackages = [ "cmd/eksctl" ];
 
-  amiID = "ami-0346bb6ef129f9f11";
   patches = [./eksctl-thar.patch ];
-  postPatch = ''
+  postPatch = let
+    tag = "${version}${lib.optionalString (versionExtra != "") "/${versionExtra}"}";
+  in ''
     substituteInPlace pkg/ami/static_resolver_ami.go \
                       --subst-var amiID
+
+    cat > pkg/version/release.go <<EOF
+    // +build release
+
+    package version
+
+    // Values of builtAt and gitCommit will be set by the linker.
+    var builtAt = "${snapshotDate}"
+    var gitCommit = "${rev}"
+    var gitTag = "${tag}"
+    EOF
   '';
 
   CGO_ENABLED=0;
-  buildFlags = [ "-tags netgo" "-tags release"
-                 "-ldflags" "-X github.com/weaveworks/eksctl/pkg/version.gitCommit=${rev} -X github.com/weaveworks/eksctl/pkg/version.builtAt=${snapshotDate} -X github.com/weaveworks/eksctl/pkg/version.gitTag=${version}" ];
+  buildFlags = [ "-tags netgo" "-tags release" ];
 
   postInstall =
   ''
--- extras/teksctl/release.nix
+++ extras/teksctl/release.nix
@@ -1,7 +1,16 @@
 { pkgs ? import <nixpkgs> {} }:
+let
+  amis = {
+    "0.1.3" = { amiID = "ami-0346bb6ef129f9f11"; };
+    "0.1.4" = { amiID = "ami-0331423be16b32cca"; };
+  };
+in
 rec {
-  teksctl = pkgs.callPackage ./. {};
-  archive = pkgs.runCommand "${teksctl.name}-archive.tar.gz" {} ''
+  teksctlFor = pkgs.callPackage ./. {};
+  archive = let
+    imageRelease = "0.1.4";
+    teksctl = teksctlFor { inherit (amis.${imageRelease}) amiID; versionExtra = "thar-${imageRelease}"; };
+  in pkgs.runCommand "${teksctl.name}-thar-${imageRelease}-archive.tar.gz" {} ''
     tar -jcf $out -C ${teksctl}/bin eksctl teksctl
   '';
 }
