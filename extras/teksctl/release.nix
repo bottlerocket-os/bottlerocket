d2918b4799183f83b38a82347f6908550ea32dd6
Jacob Vallejo <jakeev@amazon.com>
2019-10-03 14:22:54 -0700

    teksctl: build an archive to share

--- extras/teksctl/release.nix
+++ extras/teksctl/release.nix
@@ -1,4 +1,7 @@
 { pkgs ? import <nixpkgs> {} }:
-{
+rec {
   teksctl = pkgs.callPackage ./. {};
+  archive = pkgs.runCommand "${teksctl.name}-archive.tar.gz" {} ''
+    tar -jcf $out -C ${teksctl}/bin eksctl teksctl
+  '';
 }
