19a01cef8aab5f282998e5af0c4584bb77497f39
Erikson Tung <etung@amazon.com>
2019-10-29 15:40:05 -0700

    host-ctr: exposes error level log messages to console
    Splits logrus logs to different output with logsplit hook.
    Error, fatal, panic log goes to standard error and all other log levels
    go to stdout

---
+++ workspaces/host-ctr/cmd/host-ctr/logsplit.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"github.com/sirupsen/logrus"
+	"io"
+)
+
+type LogSplitHook struct {
+	output io.Writer
+	levels []logrus.Level
+}
+
+// Fire is invoked when logrus tries to log any message.
+func (hook *LogSplitHook) Fire(entry *logrus.Entry) error {
+	line, err := entry.String()
+	if err != nil {
+		return err
+	}
+	for _, level := range hook.levels {
+		if level == entry.Level {
+			_, err := hook.output.Write([]byte(line))
+			return err
+		}
+	}
+	return nil
+}
+
+// Returns the log levels this hook is being applied to
+func (hook *LogSplitHook) Levels() []logrus.Level {
+	return hook.levels
+}
