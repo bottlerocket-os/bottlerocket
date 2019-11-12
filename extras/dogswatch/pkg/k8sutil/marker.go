7e86d640bc9a419693f06633f4cac022421a9099
Jacob Vallejo <jakeev@amazon.com>
2019-11-11 16:44:36 -0800

    dogswatch: revise log to debuggable logging

--- extras/dogswatch/pkg/k8sutil/marker.go
+++ extras/dogswatch/pkg/k8sutil/marker.go
@@ -1,7 +1,9 @@
 package k8sutil
 
 import (
+	"github.com/amazonlinux/thar/dogswatch/pkg/logging"
 	"github.com/amazonlinux/thar/dogswatch/pkg/marker"
+
 	"github.com/pkg/errors"
 	v1meta "k8s.io/apimachinery/pkg/apis/meta/v1"
 	v1 "k8s.io/client-go/kubernetes/typed/core/v1"
@@ -13,12 +15,12 @@ func PostMetadata(nc v1.NodeInterface, nodeName string, cont marker.Container) e
 		return errors.WithMessage(err, "unable to get node")
 	}
 	marker.OverwriteFrom(cont, node)
-	// {
-	// 	l := logging.New("k8sutil")
-	// 	l.Debugf("annotations: %#v", node.GetAnnotations())
-	// 	l.Debugf("labels: %#v", node.GetLabels())
-	// }
-	node, err = nc.Update(node)
+	if logging.Debuggable {
+		l := logging.New("k8sutil")
+		l.Debugf("annotations: %#v", node.GetAnnotations())
+		l.Debugf("labels: %#v", node.GetLabels())
+	}
+	_, err = nc.Update(node)
 	if err != nil {
 		return errors.WithMessage(err, "unable to update node")
 	}
