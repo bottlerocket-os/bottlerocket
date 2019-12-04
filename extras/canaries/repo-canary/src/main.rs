e1da159b38ce206a45d3a18fc5d6045e4025871b
Erikson Tung <etung@amazon.com>
2019-12-04 14:24:12 -0800

    repo-canary: new `--check-upcoming-expiration`, adds README gen
    Adds a new option `--check-upcoming-expiration` that checks for any
    upcoming metadata file expiration in some specified number of days.
    
    Adds README generation from rust docstrings to the build. README
    includes the mapping of tough errors to exit codes.

--- extras/canaries/repo-canary/src/main.rs
+++ extras/canaries/repo-canary/src/main.rs
@@ -1,13 +1,48 @@
+/*!
+# Introduction
+
+`repo-canary` is a TUF repository canary that validates a specified TUF repository using [tough](https://crates.io/crates/tough).
+
+It validates by loading the repository, checking the metadata files and attempting retrieval of its listed targets.
+
+If any `tough` library error is encountered at any step of the validation process, a non-zero exit code is returned.
+Exit codes are mapped to specific `tough` library errors as follows:
+
+| `tough` error             | exit code |
+| -------------             |-------    |
+| `VerifyTrustedMetadata`   | 64        |
+| `VerifyMetadata`          | 65        |
+| `VersionMismatch`         | 66        |
+| `Transport`               | 67        |
+| `ExpiredMetadata`         | 68        |
+| `MetaMissing`             | 69        |
+| `OlderMetadata`           | 70        |
+
+
+Other exit code to errors mappings:
+
+| Other errors              | exit code |
+| -------------             |-------    |
+| Missing target in repo    | 71        |
+| Failed to download target | 72        |
+| *Metadata about to expire | 73        |
+
+(*: see `--check-upcoming-expiration-days` option in usage info)
+
+*/
+
 #![deny(rust_2018_idioms)]
 #![warn(clippy::pedantic)]
 
 #[macro_use]
 extern crate log;
 
+use chrono::{DateTime, Duration, Utc};
 use rand::seq::SliceRandom;
 use signal_hook::{iterator::Signals, SIGINT, SIGQUIT, SIGTERM};
 use simplelog::{Config as LogConfig, LevelFilter, SimpleLogger};
 use snafu::ResultExt;
+use std::collections::HashMap;
 use std::fs::{File, OpenOptions};
 use std::path::{Path, PathBuf};
 use std::str::FromStr;
@@ -22,7 +57,7 @@ static SIGNAL: AtomicI32 = AtomicI32::new(0);
 
 type HttpRepo<'a> = Repository<'a, HttpTransport>;
 
-// Custom exit codes
+// Custom exit codes to be picked up by CloudWatch event rules
 const TRUSTED_ROOT_VALIDATION_FAILURE: i32 = 64;
 const METADATA_VALIDATION_FAILURE: i32 = 65;
 const VERSION_MISMATCH: i32 = 66;
@@ -32,6 +67,8 @@ const MISSING_METADATA: i32 = 69;
 const ROLLBACK_DETECTED: i32 = 70;
 const MISSING_TARGET: i32 = 71;
 const TARGET_DOWNLOAD_FAILURE: i32 = 72;
+
+const METADATA_ABOUT_TO_EXPIRE: i32 = 73;
 const OTHER_ERROR: i32 = 1;
 
 mod error {
@@ -87,6 +124,7 @@ struct Args {
     metadata_base_url: String,
     target_base_url: String,
     trusted_root_path: PathBuf,
+    upcoming_expiration_in: u16,
     percent_target_files: u8,
 }
 
@@ -94,17 +132,22 @@ struct Args {
 fn usage() -> ! {
     let program_name = env::args().next().unwrap_or_else(|| "program".to_string());
     eprintln!(
-        r"Usage: {}
-            --metadata-base-url URL
-            --target-base-url URL
-            --trusted-root-path PATH_TO_root.json
-            [ --percentage-of-targets-to-retrieve 0-100 ] 'Randomly samples specified percentage of targets'
-            [ --log-level trace|debug|info|warn|error ]
+        r"TUF Repository canary. Non-zero exit codes are mapped to specific `tough` library errors.
 
-            If --percentage-of-targets-to-retrieve is not specified, {} will attempt to retrieve all targets listed in the TUF repository.
+        USAGE:
+            {} <REQUIRED> <OPTIONS>
+
+        REQUIRED:
+            --metadata-base-url URL                 The TUF repository metadata base URL
+            --target-base-url URL                   The TUF repository targets base URL
+            --trusted-root-path PATH/TO/root.json   Path to the trusted root.json
+
+        OPTIONS:
+            [ --check-upcoming-expiration-days 0-365 ]      Outputs a list of metadata files expiring within specified number of days (default 0 - don't check). Short-curcuits and exits non-zero if there are any.
+            [ --percentage-targets-to-retrieve 0-100 ]      Randomly samples and retrieves specified percentage of targets (default 100)
+            [ --log-level trace|debug|info|warn|error ]     Specifies logging level (default info)
         ",
         program_name,
-        program_name
     );
     process::exit(2);
 }
@@ -118,6 +161,7 @@ fn usage_msg<S: AsRef<str>>(msg: S) -> ! {
 /// Parse the args to the program and return an Args struct
 fn parse_args(args: env::Args) -> Args {
     let mut log_level = None;
+    let mut upcoming_expiration_in = None;
     let mut metadata_base_url = None;
     let mut percent_target_files: Option<u8> = None;
     let mut target_base_url = None;
@@ -150,7 +194,21 @@ fn parse_args(args: env::Args) -> Args {
                 )
             }
 
-            "--percentage-of-targets-to-retrieve" => {
+            "--check-upcoming-expiration-days" => {
+                let days = iter
+                    .next()
+                    .unwrap_or_else(|| {
+                        usage_msg("Did not give argument (days) to --check-upcoming-expiration-days")
+                    })
+                    .parse::<u16>()
+                    .unwrap_or_else(|_| usage_msg("Invalid argument: expecting days from 1-365"));
+                if days > 365 {
+                    usage_msg("Invalid argument: expecting days from 1 to 365")
+                }
+                upcoming_expiration_in = Some(days);
+            }
+
+            "--percentage-targets-to-retrieve" => {
                 let percentage = iter
                     .next()
                     .unwrap_or_else(|| {
@@ -182,6 +240,7 @@ fn parse_args(args: env::Args) -> Args {
     Args {
         log_level: log_level.unwrap_or_else(|| LevelFilter::Info),
         metadata_base_url: metadata_base_url.unwrap_or_else(|| usage()),
+        upcoming_expiration_in: upcoming_expiration_in.unwrap_or_else(|| 0),
         percent_target_files: percent_target_files.unwrap_or_else(|| 100),
         target_base_url: target_base_url.unwrap_or_else(|| usage()),
         trusted_root_path: trusted_root_path.unwrap_or_else(|| usage()),
@@ -213,8 +272,8 @@ fn retrieve_percentage_of_targets<P>(
 where
     P: AsRef<Path>,
 {
-    let targets = repo.targets();
-    let percentage = percentage as f32 / 100.0;
+    let targets = &repo.targets().signed.targets;
+    let percentage = f32::from(percentage) / 100.0;
     let num_to_retrieve = (targets.len() as f32 * percentage).ceil();
     let mut rng = &mut rand::thread_rng();
     let mut sampled_targets: Vec<String> = targets.keys().map(|key| key.to_string()).collect();
@@ -252,6 +311,35 @@ where
         };
     }
     Ok(0)
+}
+
+/// Checks for upcoming role expirations, gathers them in a list along with their expiration date.
+fn find_upcoming_metadata_expiration(
+    repo: &HttpRepo<'_>,
+    days: u16,
+) -> HashMap<tough::schema::RoleType, DateTime<Utc>> {
+    let mut expirations = HashMap::new();
+    let time_limit = Utc::now() + Duration::days(i64::from(days));
+    info!(
+        "Looking for metadata expirations happening from now to {:?}",
+        time_limit
+    );
+    if repo.root().signed.expires <= time_limit {
+        expirations.insert(tough::schema::RoleType::Root, repo.root().signed.expires);
+    }
+    if repo.snapshot().signed.expires <= time_limit {
+        expirations.insert(
+            tough::schema::RoleType::Snapshot,
+            repo.snapshot().signed.expires,
+        );
+    }
+    if repo.timestamp().signed.expires <= time_limit {
+        expirations.insert(
+            tough::schema::RoleType::Timestamp,
+            repo.timestamp().signed.expires,
+        );
+    }
+    expirations
 }
 
 fn main() -> Result<()> {
@@ -292,16 +380,38 @@ fn main() -> Result<()> {
         },
     );
     // Check for errors from loading the TUF repository
-    let rc = match &repo {
-        Err(err) => match_report_tough_error(err),
+    let mut rc = 0;
+    match &repo {
+        Err(err) => rc = match_report_tough_error(err),
         Ok(repo) => {
             info!("Loaded TUF repo");
-            // Try retrieving listed targets
-            info!(
-                "Downloading {}% of listed targets",
-                args.percent_target_files
-            );
-            retrieve_percentage_of_targets(repo, datastore.path(), args.percent_target_files)?
+            if args.upcoming_expiration_in != 0 {
+                // Check for upcoming metadata expirations
+                let upcoming_expirations =
+                    find_upcoming_metadata_expiration(repo, args.upcoming_expiration_in);
+                if !upcoming_expirations.is_empty() {
+                    for (role, date) in upcoming_expirations {
+                        warn!(
+                            "{} expiring within {} day(s) on {}",
+                            role, args.upcoming_expiration_in, date
+                        )
+                    }
+                    rc = METADATA_ABOUT_TO_EXPIRE;
+                }
+            }
+            // If there are no errors so far
+            if rc == 0 {
+                // Try retrieving listed targets
+                info!(
+                    "Downloading {}% of listed targets",
+                    args.percent_target_files
+                );
+                rc = retrieve_percentage_of_targets(
+                    repo,
+                    datastore.path(),
+                    args.percent_target_files,
+                )?
+            }
         }
     };
 
