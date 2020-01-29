7146ea0dbcf63a99763027754f311be51e4f0096
Tom Kirchner <tjk@amazon.com>
2020-01-29 11:57:33 -0800

    Add helpers for common add/remove setting migrations

--- workspaces/api/migration/migrations/v0.2/remove-region/src/main.rs
+++ workspaces/api/migration/migrations/v0.2/remove-region/src/main.rs
@@ -1,33 +1,13 @@
 #![deny(rust_2018_idioms)]
 
-use migration_helpers::{migrate, Migration, MigrationData, Result};
+use migration_helpers::{migrate, Result};
+use migration_helpers::common_migrations::AddSettingMigration;
 use std::process;
 
 /// We added a generated setting, "settings.aws.region", and want to make sure it's removed before
 /// we go back to old versions that don't understand it.
-struct RemoveRegionMigration;
-
-impl Migration for RemoveRegionMigration {
-    /// New versions will generate the setting, we don't need to do anything.
-    fn forward(&mut self, input: MigrationData) -> Result<MigrationData> {
-        println!("RemoveRegionMigration has no work to do on upgrade.");
-        Ok(input)
-    }
-
-    /// Older versions don't know about region; we remove it so that old versions don't see it and
-    /// fail deserialization.  (The setting is generated in new versions, and safe to remove.)
-    fn backward(&mut self, mut input: MigrationData) -> Result<MigrationData> {
-        if let Some(region) = input.data.remove("settings.aws.region") {
-            println!("Removed settings.aws.region, which was set to '{}'", region);
-        } else {
-            println!("Found no settings.aws.region to remove");
-        }
-        Ok(input)
-    }
-}
-
 fn run() -> Result<()> {
-    migrate(RemoveRegionMigration)
+    migrate(AddSettingMigration("settings.aws.region"))
 }
 
 // Returning a Result from main makes it print a Debug representation of the error, but with Snafu
