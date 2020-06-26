44b649944bc296f0e6b5e2d6f454fc528a3f05f8
Samuel Karp <skarp@amazon.com>
2020-06-25 18:03:28 -0700

    ecs-settings-applier: use snafu for errors

--- sources/api/ecs-settings-applier/src/main.rs
+++ sources/api/ecs-settings-applier/src/main.rs
@@ -1,11 +1,15 @@
-use log::{debug, error};
+use log::debug;
+use std::{process};
 use std::fs;
-use std::io;
-use std::path::{Path, PathBuf};
+use std::path::{Path};
 use serde::{Serialize};
+use snafu::{ResultExt};
 
 // FIXME Get from configuration in the future
 const DEFAULT_API_SOCKET: &str = "/run/api.sock";
+
+const DEFAULT_ECS_CONFIG_PATH: &str = "/etc/ecs/ecs.config.json";
+
 
 #[derive(Serialize, Debug)]
 #[serde(rename_all="PascalCase")]
@@ -14,50 +18,61 @@ struct ECSConfig {
     cluster: Option<String>,
 }
 
+// Returning a Result from main makes it print a Debug representation of the error, but with Snafu
+// we have nice Display representations of the error, so we wrap "main" (run) and print any error.
+// https://github.com/shepmaster/snafu/issues/110
 fn main() {
-    // Get all settings values for config file templates
-    debug!("Requesting settings values");
-    let settings = match schnauzer::get_settings(&DEFAULT_API_SOCKET) {
-        Ok(s) => s,
-        Err(e) => {
-            error!("Failed to read settings: {}", e);
-            return
-        }
-    };
-
-    debug!("settings = {:#?}", settings.settings);
-    let config = ECSConfig{ cluster: settings.settings.and_then(|s| s.ecs).and_then(|s| s.cluster)};
-    let serialized = serde_json::to_string(&config).unwrap();
-    debug!("serialized = {}", serialized);
-
-    let config_path = PathBuf::from("/etc/ecs/ecs.config.json");
-    match write_to_disk(config_path, serialized) {
-        Some(e) => {
-            error!("Error! {}", e)
-            // TODO exit
-        }
-        _ => {}
+    if let Err(e) = run() {
+        eprintln!("{}", e);
+        process::exit(1);
     }
 }
 
+fn run() -> Result<()> {
+    // Get all settings values for config file templates
+    debug!("Requesting settings values");
+    let settings = schnauzer::get_settings(&DEFAULT_API_SOCKET).context(error::Settings)?;
+
+    debug!("settings = {:#?}", settings.settings);
+    let config = ECSConfig{ cluster: settings.settings.and_then(|s| s.ecs).and_then(|s| s.cluster)};
+    let serialized = serde_json::to_string(&config).context(error::Serialization)?;
+    debug!("serialized = {}", serialized);
+
+    write_to_disk(DEFAULT_ECS_CONFIG_PATH, serialized).context(error::FS{path:DEFAULT_ECS_CONFIG_PATH})?;
+    Ok(())
+}
+
 /// Writes the rendered data at the proper location
-fn write_to_disk<P: AsRef<Path>, C: AsRef<[u8]>>(path: P, contents: C) -> Option<io::Error> {
+fn write_to_disk<P: AsRef<Path>, C: AsRef<[u8]>>(path: P, contents: C) -> std::io::Result<()> {
     if let Some(dirname) = path.as_ref().parent() {
-        let result = fs::create_dir_all(dirname);
-        match result {
-            Err(e) => {
-                return Some(e)
-            }
-            _ => {}
-        };
+        fs::create_dir_all(dirname)?;
     };
 
-    return match fs::write(path, contents) {
-        Err(e) => {
-            Some(e)
-        }
-        _ => {
-            None
+    fs::write(path, contents)
+}
+
+type Result<T> = std::result::Result<T, error::Error>;
+
+mod error {
+    use snafu::Snafu;
+
+    #[derive(Debug, Snafu)]
+    #[snafu(visibility = "pub(super)")]
+    pub(super) enum Error {
+        #[snafu(display("Failed to read settings: {}", source))]
+        Settings{
+            source: schnauzer::Error
+        },
+
+        #[snafu(display("Failed to serialize ECS config: {}", source))]
+        Serialization{
+            source: serde_json::error::Error
+        },
+
+        #[snafu(display("Filesystem operation for path {} failed: {}", path, source))]
+        FS{
+            path: &'static str,
+            source: std::io::Error
         }
     }
 }
