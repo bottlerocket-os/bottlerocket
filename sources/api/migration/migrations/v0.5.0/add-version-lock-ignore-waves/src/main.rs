370a58590c9a5a038652409af1886c54f8f22016
Erikson Tung <etung@amazon.com>
2020-06-29 12:01:27 -0700

    migrations: migration to add 'version-lock' & 'ignore-waves' settings
    Adds a migration for adding two new update settings: 'version-lock' and
    'ignore-waves'

---
+++ sources/api/migration/migrations/v0.5.0/add-version-lock-ignore-waves/src/main.rs
@@ -0,0 +1,23 @@
+#![deny(rust_2018_idioms)]
+
+use migration_helpers::common_migrations::AddSettingsMigration;
+use migration_helpers::{migrate, Result};
+use std::process;
+
+/// We added two new settings, `updates.version-lock` and `updates.ignore-waves`
+fn run() -> Result<()> {
+    migrate(AddSettingsMigration(&[
+        "settings.updates.version-lock",
+        "settings.updates.ignore-waves",
+    ]))
+}
+
+// Returning a Result from main makes it print a Debug representation of the error, but with Snafu
+// we have nice Display representations of the error, so we wrap "main" (run) and print any error.
+// https://github.com/shepmaster/snafu/issues/110
+fn main() {
+    if let Err(e) = run() {
+        eprintln!("{}", e);
+        process::exit(1);
+    }
+}
