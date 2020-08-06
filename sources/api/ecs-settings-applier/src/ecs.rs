6a6cdf44aa01046b9a80594753119aaaeaee2b35
Samuel Karp <skarp@amazon.com>
2020-08-06 10:48:46 -0700

    ecs-settings-applier: add link to ECS agent config

--- sources/api/ecs-settings-applier/src/ecs.rs
+++ sources/api/ecs-settings-applier/src/ecs.rs
@@ -4,7 +4,8 @@
 ecs-settings-applier generates a configuration file for the ECS agent from Bottlerocket settings.
 
 The configuration file for ECS is a JSON-formatted document with conditionally-defined keys and
-embedded lists.
+embedded lists.  The structure and names of fields in the document can be found
+[here](https://github.com/aws/amazon-ecs-agent/blob/a250409cf5eb4ad84a7b889023f1e4d2e274b7ab/agent/config/types.go).
 */
 use log::debug;
 use serde::Serialize;
