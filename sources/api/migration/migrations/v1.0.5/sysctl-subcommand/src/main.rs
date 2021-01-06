76e394151216a62a7a0160a46fd39e4c0bc939ff
Erikson Tung <etung@amazon.com>
2021-01-05 16:03:26 -0800

    migration-helpers: ReplaceListMigration -> ReplaceListsMigration
    ReplaceListMigration becomes ReplaceListsMigration.
    ReplaceListsMigration now can handle multiple settings that contain
    lists to be replaced.

--- sources/api/migration/migrations/v1.0.5/sysctl-subcommand/src/main.rs
+++ sources/api/migration/migrations/v1.0.5/sysctl-subcommand/src/main.rs
@@ -1,17 +1,17 @@
 #![deny(rust_2018_idioms)]
 
-use migration_helpers::common_migrations::ReplaceListMigration;
+use migration_helpers::common_migrations::{ListReplacement, ReplaceListsMigration};
 use migration_helpers::{migrate, Result};
 use std::process;
 
 /// We changed corndog to use subcommands so it can handle different kernel settings without having
 /// to apply them all every time.
 fn run() -> Result<()> {
-    migrate(ReplaceListMigration {
+    migrate(ReplaceListsMigration(vec![ListReplacement {
         setting: "services.sysctl.restart-commands",
         old_vals: &["/usr/bin/corndog"],
         new_vals: &["/usr/bin/corndog sysctl"],
-    })
+    }]))
 }
 
 // Returning a Result from main makes it print a Debug representation of the error, but with Snafu
