7a1005324ca4b7d223479a2c9ade7e01218e766a
Erikson Tung <etung@amazon.com>
2021-01-05 17:45:43 -0800

    migrations: adds necessary migrations for `settings.network`
    Adds new migrations for migrating new settings and affected service
    settings. We also added new service settings for docker and
    host-containerd to incorporate restart commands for those services.

---
+++ sources/api/migration/migrations/v1.0.5/add-network-settings/src/main.rs
@@ -0,0 +1,25 @@
+#![deny(rust_2018_idioms)]
+
+use migration_helpers::common_migrations::AddPrefixesMigration;
+use migration_helpers::{migrate, Result};
+use std::process;
+
+/// We added a set of settings for configuring service network behavior and their associated
+/// configuration file. Remove the whole `settings.network`, `configuration-files.proxy-env` prefix
+/// if we downgrade.
+fn run() -> Result<()> {
+    migrate(AddPrefixesMigration(vec![
+        "settings.network",
+        "configuration-files.proxy-env",
+    ]))
+}
+
+// Returning a Result from main makes it print a Debug representation of the error, but with Snafu
+// we have nice Display representations of the error, so we wrap "main" (run) and print any error.
+// https://github.com/shepmaster/snafu/issues/110
+fn main() {
+    if let Err(e) = run() {
+        eprintln!("{}", e);
+        process::exit(1);
+    }
+}
