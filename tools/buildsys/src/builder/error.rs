32a76fc205c6ff6d1c3ced20e61cb60837fe7f06
Ben Cressey <bcressey@amazon.com>
2021-01-20 16:52:16 +0000

    build: track and clean output files
    This adds a layer of indirection between the build artifacts and the
    final output directory, so we can keep track of what we've previously
    built for a given package or variant.
    
    That allows us to remove the files before the next build, so they do
    not interact with other builds in unexpected ways.
    
    Signed-off-by: Ben Cressey <bcressey@amazon.com>

--- tools/buildsys/src/builder/error.rs
+++ tools/buildsys/src/builder/error.rs
@@ -16,6 +16,37 @@ pub(crate) enum Error {
         source: std::io::Error,
     },
 
+    #[snafu(display("Failed to get filename for '{}'", path.display()))]
+    BadFilename { path: PathBuf },
+
+    #[snafu(display("Failed to create directory '{}': {}", path.display(), source))]
+    DirectoryCreate {
+        path: PathBuf,
+        source: std::io::Error,
+    },
+
+    #[snafu(display("Failed to walk directory to find marker files: {}", source))]
+    DirectoryWalk { source: walkdir::Error },
+
+    #[snafu(display("Failed to create file '{}': {}", path.display(), source))]
+    FileCreate {
+        path: PathBuf,
+        source: std::io::Error,
+    },
+
+    #[snafu(display("Failed to remove file '{}': {}", path.display(), source))]
+    FileRemove {
+        path: PathBuf,
+        source: std::io::Error,
+    },
+
+    #[snafu(display("Failed to rename file '{}' to '{}': {}", old_path.display(), new_path.display(), source))]
+    FileRename {
+        old_path: PathBuf,
+        new_path: PathBuf,
+        source: std::io::Error,
+    },
+
     #[snafu(display("Missing environment variable '{}'", var))]
     Environment {
         var: String,
