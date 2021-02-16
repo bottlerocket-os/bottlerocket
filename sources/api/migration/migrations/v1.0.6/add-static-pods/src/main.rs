de6926b7d841bffd648523eb89d55b57178cc3fc
Erikson Tung <etung@amazon.com>
2021-02-16 10:41:34 -0800

    migrations: add `add-static-pods` migration
    Adds a new migration for new k8s static-pods settings

---
+++ sources/api/migration/migrations/v1.0.6/add-static-pods/src/main.rs
@@ -0,0 +1,24 @@
+#![deny(rust_2018_idioms)]
+
+use migration_helpers::common_migrations::AddPrefixesMigration;
+use migration_helpers::{migrate, Result};
+use std::process;
+
+/// We added new settings for defining k8s static pods.
+/// Remove `settings.kubernetes.static-pods`, `services.static-pods` prefixes when we downgrade.
+fn run() -> Result<()> {
+    migrate(AddPrefixesMigration(vec![
+        "settings.kubernetes.static-pods",
+        "services.static-pods",
+    ]))
+}
+
+// Returning a Result from main makes it print a Debug representation of the error, but with Snafu
+// we have nice Display representations of the error, so we wrap "main" (run) and print any error.
+// https://github.com/shepmaster/snafu/issues/110
+fn main() {
+    if let Err(e) = run() {
+        eprintln!("{}", e);
+        process::exit(1);
+    }
+}
