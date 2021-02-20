762a4bb6f3d2456beaabce1c23f67261e9e8d466
Ben Cressey <bcressey@amazon.com>
2021-02-20 20:03:39 +0000

    add migrations for kubelet standalone mode and TLS auth
    Signed-off-by: Ben Cressey <bcressey@amazon.com>

---
+++ sources/api/migration/migrations/v1.0.6/kubelet-standalone-tls-settings/src/main.rs
@@ -0,0 +1,28 @@
+#![deny(rust_2018_idioms)]
+
+use migration_helpers::common_migrations::AddPrefixesMigration;
+use migration_helpers::{migrate, Result};
+use std::process;
+
+/// We added new settings for running kubelet in standalone mode, and for using TLS auth.
+/// We also added new configuration files to apply these settings. They need to be removed
+/// when we downgrade.
+fn run() -> Result<()> {
+    migrate(AddPrefixesMigration(vec![
+        "settings.kubernetes.bootstrap-token",
+        "settings.kubernetes.authentication-mode",
+        "settings.kubernetes.standalone-mode",
+        "configuration-files.kubelet-bootstrap-kubeconfig",
+        "configuration-files.kubelet-exec-start-conf",
+    ]))
+}
+
+// Returning a Result from main makes it print a Debug representation of the error, but with Snafu
+// we have nice Display representations of the error, so we wrap "main" (run) and print any error.
+// https://github.com/shepmaster/snafu/issues/110
+fn main() {
+    if let Err(e) = run() {
+        eprintln!("{}", e);
+        process::exit(1);
+    }
+}
