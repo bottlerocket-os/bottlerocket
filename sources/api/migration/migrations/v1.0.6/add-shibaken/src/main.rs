8095c02d7088c8fe8fbc0665773120e89050362e
Patrick J.P. Culp <jpculp@amazon.com>
2021-02-24 19:53:12 +0000

    migrations: add `add-shibaken` migration
    Adds a new migration for shibaken setting-generator

---
+++ sources/api/migration/migrations/v1.0.6/add-shibaken/src/main.rs
@@ -0,0 +1,23 @@
+#![deny(rust_2018_idioms)]
+
+use migration_helpers::common_migrations::{AddMetadataMigration, SettingMetadata};
+use migration_helpers::{migrate, Result};
+use std::process;
+
+/// We added a new setting metadata, `host-containers.admin.user-data.setting-generator`
+fn run() -> Result<()> {
+    migrate(AddMetadataMigration(&[SettingMetadata {
+        metadata: &["setting-generator"],
+        setting: "settings.host-containers.admin.user-data",
+    }]))
+}
+
+// Returning a Result from main makes it print a Debug representation of the error, but with Snafu
+// we have nice Display representations of the error, so we wrap "main" (run) and print any error.
+// https://github.com/shepmaster/snafu/issues/110
+fn main() {
+    if let Err(e) = run() {
+        eprintln!("{}", e);
+        process::exit(1);
+    }
+}
