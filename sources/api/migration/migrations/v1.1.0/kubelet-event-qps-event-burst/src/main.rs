f65be1c280736a368fc4bff6bbd284fed302e6d4
Tianhao Geng <tianhg@amazon.com>
2021-04-28 02:07:26 +0000

    migrations: add migration for adding event-qps and event-burst
    Adds a migration for two new settings `settings.kubernetes.event-qps`
    and `settings.kubernetes.event-burst`

---
+++ sources/api/migration/migrations/v1.1.0/kubelet-event-qps-event-burst/src/main.rs
@@ -0,0 +1,24 @@
+#![deny(rust_2018_idioms)]
+
+use migration_helpers::common_migrations::AddSettingsMigration;
+use migration_helpers::{migrate, Result};
+use std::process;
+
+/// We added two new settings for configuring kubelet, `settings.kubernetes.event-qps`
+/// and `settings.kubernetes.event-burst`
+fn run() -> Result<()> {
+    migrate(AddSettingsMigration(&[
+        "settings.kubernetes.event-qps",
+        "settings.kubernetes.event-burst",
+    ]))
+}
+
+// Returning a Result from main makes it print a Debug representation of the error, but with Snafu
+// we have nice Display representations of the error, so we wrap "main" (run) and print any error.
+// https://github.com/shepmaster/snafu/issues/110
+fn main() {
+    if let Err(e) = run() {
+        eprintln!("{}", e);
+        process::exit(1);
+    }
+}
