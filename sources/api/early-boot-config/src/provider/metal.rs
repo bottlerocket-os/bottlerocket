218e47481f31fc335aece321fe0b6bcd4991d451
Zac Mrowicki <mrowicki@amazon.com>
2021-12-09 17:53:40 +0000

    early-boot-config: Add metal provider
    This change adds an additional provider in `early-boot-config` that is
    meant for variants that run on bare metal.  It looks for user data at a
    specific location on the filesystem.

---
+++ sources/api/early-boot-config/src/provider/metal.rs
@@ -0,0 +1,25 @@
+//! The metal module implements the `PlatformDataProvider` trait for gathering userdata on bare
+//! metal.
+
+use super::{PlatformDataProvider, SettingsJson};
+use async_trait::async_trait;
+
+use crate::provider::local_file::{local_file_user_data, USER_DATA_FILE};
+
+pub(crate) struct MetalDataProvider;
+
+#[async_trait]
+impl PlatformDataProvider for MetalDataProvider {
+    async fn platform_data(
+        &self,
+    ) -> std::result::Result<Vec<SettingsJson>, Box<dyn std::error::Error>> {
+        let mut output = Vec::new();
+
+        match local_file_user_data()? {
+            None => warn!("No user data found via local file: {}", USER_DATA_FILE),
+            Some(s) => output.push(s),
+        }
+
+        Ok(output)
+    }
+}
