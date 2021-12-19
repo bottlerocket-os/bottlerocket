6f1b7fd0dc73b0743e1177694aaf427bd6453273
Josh Soref <jsoref@users.noreply.github.com>
2021-12-19 12:45:08 -0500

    spelling: configuration
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- sources/api/bootstrap-containers/src/main.rs
+++ sources/api/bootstrap-containers/src/main.rs
@@ -9,7 +9,7 @@ It queries the API for their settings, then configures the system by:
   user-data setting is set for the host container.  (The decoded contents are available to the
   container at /.bottlerocket/bootstrap-containers/<name>/user-data)
 * creating an environment file used by a bootstrap-container-specific instance of a systemd service
-* creating a systemd drop-in configureation file used by a bootstrap-container-specific
+* creating a systemd drop-in configuration file used by a bootstrap-container-specific
 instance of a systemd service
 * ensuring that the bootstrap container's systemd service is enabled/disabled for the next boot
 
