8dfac508a5a3e100d000708d3c6b7c37a5f0f137
Sean P. Kelly <seankell@amazon.com>
2022-05-05 21:31:19 +0000

    migrations: add migrations for `settings.network.hosts`

---
+++ sources/api/migration/migrations/v1.8.0/etc-hosts/src/main.rs
@@ -0,0 +1,20 @@
+#![deny(rust_2018_idioms)]
+
+use migration_helpers::common_migrations::AddPrefixesMigration;
+use migration_helpers::{migrate, Result};
+use std::process;
+
+/// We added a new setting under `settings.network` for configuring /etc/hosts: `settings.network.hosts`
+fn run() -> Result<()> {
+    migrate(AddPrefixesMigration(vec!["settings.network.hosts"]))
+}
+
+// Returning a Result from main makes it print a Debug representation of the error, but with Snafu
+// we have nice Display representations of the error, so we wrap "main" (run) and print any error.
+// https://github.com/shepmaster/snafu/issues/110
+fn main() {
+    if let Err(e) = run() {
+        eprintln!("{}", e);
+        process::exit(1);
+    }
+}
