228d98161c61b4fed3c1e081678678fdf5be9442
Zac Mrowicki <mrowicki@amazon.com>
2022-08-16 16:13:26 +0000

    migrations: Add migrations for `settings.dns`
    This adds the migrations needed for `settings.dns`, one for the setting,
    its service and configuration file, and one for the setting metadata.

---
+++ sources/api/migration/migrations/v1.10.0/dns-settings/src/main.rs
@@ -0,0 +1,24 @@
+#![deny(rust_2018_idioms)]
+
+use migration_helpers::common_migrations::AddPrefixesMigration;
+use migration_helpers::{migrate, Result};
+use std::process;
+
+/// We added new settings under `settings.dns` for configuring /etc/resolv.conf
+fn run() -> Result<()> {
+    migrate(AddPrefixesMigration(vec![
+        "settings.dns",
+        "services.dns",
+        "configuration-files.netdog-toml",
+    ]))
+}
+
+// Returning a Result from main makes it print a Debug representation of the error, but with Snafu
+// we have nice Display representations of the error, so we wrap "main" (run) and print any error.
+// https://github.com/shepmaster/snafu/issues/110
+fn main() {
+    if let Err(e) = run() {
+        eprintln!("{}", e);
+        process::exit(1);
+    }
+}
