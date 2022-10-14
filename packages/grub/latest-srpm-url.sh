61517d2be14e72a5a88f2ccd621ad8f0df9c7f0b
Markus Boehme <markubo@amazon.com>
2022-10-14 20:14:48 +0000

    grub: update to grub-2.06-42.amzn2022
    Update GRUB to grub-2.06-42.amzn2022. This also switches Bottlerocket's
    immediate upstream from Amazon Linux 2 to Amazon Linux 2022, which more
    closely tracks the GRUB project.
    
    Signed-off-by: Markus Boehme <markubo@amazon.com>

--- packages/grub/latest-srpm-url.sh
+++ packages/grub/latest-srpm-url.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
-docker run --rm amazonlinux:2 sh -c 'yum install -q -y yum-utils && yumdownloader -q --source --urls grub2 | grep ^http'
+cmd='dnf install -q -y --releasever=latest yum-utils && yumdownloader -q --releasever=latest --source --urls grub2'
+docker run --rm amazonlinux:2022 sh -c "${cmd}" \
+    | grep '^http' \
+    | xargs --max-args=1 --no-run-if-empty realpath --canonicalize-missing --relative-to=. \
+    | sed 's_:/_://_'
