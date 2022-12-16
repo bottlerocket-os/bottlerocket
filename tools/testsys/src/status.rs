1a8889a82de913e2ff0f957e427e3e0dba66f61b
ecpullen <ecpullen@aol.com>
2022-12-16 16:13:42 +0000

    testsys: Improve filtering of `watch-test`

--- tools/testsys/src/status.rs
+++ tools/testsys/src/status.rs
@@ -1,7 +1,7 @@
 use crate::error::{self, Result};
 use clap::Parser;
 use log::{debug, info};
-use model::test_manager::{SelectionParams, TestManager};
+use model::test_manager::{CrdState, CrdType, SelectionParams, TestManager};
 use snafu::ResultExt;
 
 /// Check the status of testsys objects.
@@ -22,10 +22,36 @@ pub(crate) struct Status {
     /// Focus status on a particular variant
     #[clap(long)]
     variant: Option<String>,
+
+    /// Only show tests
+    #[clap(long)]
+    test: bool,
+
+    /// Only show passed tests
+    #[clap(long, conflicts_with_all=&["failed", "running"])]
+    passed: bool,
+
+    /// Only show failed tests
+    #[clap(long, conflicts_with_all=&["passed", "running"])]
+    failed: bool,
+
+    /// Only CRD's that haven't finished
+    #[clap(long, conflicts_with_all=&["passed", "failed"])]
+    running: bool,
 }
 
 impl Status {
     pub(crate) async fn run(self, client: TestManager) -> Result<()> {
+        let state = if self.running {
+            Some(CrdState::NotFinished)
+        } else if self.passed {
+            Some(CrdState::Passed)
+        } else if self.failed {
+            Some(CrdState::Failed)
+        } else {
+            None
+        };
+        let crd_type = self.test.then_some(CrdType::Test);
         let mut labels = Vec::new();
         if let Some(arch) = self.arch {
             labels.push(format!("testsys/arch={}", arch))
@@ -37,6 +63,8 @@ impl Status {
             .status(
                 &SelectionParams {
                     labels: Some(labels.join(",")),
+                    state,
+                    crd_type,
                     ..Default::default()
                 },
                 self.controller,
