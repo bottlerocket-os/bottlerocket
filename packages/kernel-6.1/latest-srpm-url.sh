a538266bf0c599cd5aeb98573732d4b2d0987190
Leonard Foerster <foersleo@amazon.com>
2023-03-08 16:08:55 +0000

    packages: Add kernel-6.1 sources
    Signed-off-by: Leonard Foerster <foersleo@amazon.com>

---
+++ packages/kernel-6.1/latest-srpm-url.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+cmd='dnf install -q -y --releasever=latest yum-utils && yumdownloader -q --releasever=latest --source --urls kernel'
+docker run --rm amazonlinux:2023 sh -c "${cmd}" \
+    | grep '^http' \
+    | xargs --max-args=1 --no-run-if-empty realpath --canonicalize-missing --relative-to=. \
+    | sed 's_:/_://_'
