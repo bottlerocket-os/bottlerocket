e4229f4da1169e988d72db5e6d09c9a810e2fe0e
Sean McGinnis <stmcg@amazon.com>
2023-03-28 20:39:48 +0000

    bloodhound: Add Bottlerocket 3.4.2.2 check
    Adds CIS 3.4.2.2 to verify IPv6 koopback traffic is configured.
    
    Signed-off-by: Sean McGinnis <stmcg@amazon.com>

--- sources/bloodhound/src/bin/bottlerocket-checks/main.rs
+++ sources/bloodhound/src/bin/bottlerocket-checks/main.rs
@@ -54,6 +54,7 @@ fn main() {
             level: 1,
         }),
         "br03040201" => Box::new(BR03040201Checker {}),
+        "br03040202" => Box::new(BR03040202Checker {}),
         "br03040203" => Box::new(ManualChecker {
             name: cmd_name.to_string(),
             title: "Ensure IPv6 outbound and established connections are configured".to_string(),
