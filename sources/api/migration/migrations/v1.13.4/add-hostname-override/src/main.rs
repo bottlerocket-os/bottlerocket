c2a6de32b09d5c183d08d95402d6cec8bd4b668f
Erikson Tung <etung@amazon.com>
2023-04-18 17:19:05 -0700

    migrations: add migrations for new 'hostname-override' setting

---
+++ sources/api/migration/migrations/v1.13.4/add-hostname-override/src/main.rs
@@ -0,0 +1,20 @@
+use migration_helpers::common_migrations::AddSettingsMigration;
+use migration_helpers::{migrate, Result};
+use std::process;
+
+/// We added a new setting for specifying the '--hostname-override' kubelet option for kubernetes variants
+fn run() -> Result<()> {
+    migrate(AddSettingsMigration(&[
+        "settings.kubernetes.hostname-override",
+    ]))
+}
+
+// Returning a Result from main makes it print a Debug representation of the error, but with Snafu
+// we have nice Display representations of the error, so we wrap "main" (run) and print any error.
+// https://github.com/shepmaster/snafu/issues/110
+fn main() {
+    if let Err(e) = run() {
+        eprintln!("{}", e);
+        process::exit(1);
+    }
+}
