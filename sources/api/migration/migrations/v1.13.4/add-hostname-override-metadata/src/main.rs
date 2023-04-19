c945984904e6d71db79fc87f989f8974cce1abd0
Erikson Tung <etung@amazon.com>
2023-04-18 17:19:05 -0700

    migrations: add migrations for new 'hostname-override' setting

---
+++ sources/api/migration/migrations/v1.13.4/add-hostname-override-metadata/src/main.rs
@@ -0,0 +1,21 @@
+use migration_helpers::common_migrations::{AddMetadataMigration, SettingMetadata};
+use migration_helpers::{migrate, Result};
+use std::process;
+
+/// We added the `setting-generator` metadata for `kubernetes.hostname-override`
+fn run() -> Result<()> {
+    migrate(AddMetadataMigration(&[SettingMetadata {
+        metadata: &["setting-generator"],
+        setting: "settings.kubernetes.hostname-override",
+    }]))
+}
+
+// Returning a Result from main makes it print a Debug representation of the error, but with Snafu
+// we have nice Display representations of the error, so we wrap "main" (run) and print any error.
+// https://github.com/shepmaster/snafu/issues/110
+fn main() {
+    if let Err(e) = run() {
+        eprintln!("{}", e);
+        process::exit(1);
+    }
+}
