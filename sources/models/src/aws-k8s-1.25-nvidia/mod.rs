4cffcefe5c84757f70d88a481b3f745e7a0ef3b3
Erikson Tung <etung@amazon.com>
2023-04-18 17:00:03 -0700

    models: generate 'hostname-override' and set cp to external in aws-k8s-1.26
    This separates the model for aws-k8s-1.26 so that we generate the
    'hostname-override' setting and set the cloud-provider to 'external' for
    aws-k8s-1.26 variants only.

---
+++ sources/models/src/aws-k8s-1.25-nvidia/mod.rs
@@ -0,0 +1,35 @@
+use crate::modeled_types::Identifier;
+use crate::{
+    AutoScalingSettings, AwsSettings, BootSettings, BootstrapContainer, CloudFormationSettings,
+    DnsSettings, HostContainer, KernelSettings, KubernetesSettings, MetricsSettings,
+    NetworkSettings, NtpSettings, OciDefaults, OciHooks, PemCertificate, RegistrySettings,
+    UpdatesSettings,
+};
+
+use model_derive::model;
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+
+// Note: we have to use 'rename' here because the top-level Settings structure is the only one
+// that uses its name in serialization; internal structures use the field name that points to it
+#[model(rename = "settings", impl_default = true)]
+struct Settings {
+    motd: String,
+    kubernetes: KubernetesSettings,
+    updates: UpdatesSettings,
+    host_containers: HashMap<Identifier, HostContainer>,
+    bootstrap_containers: HashMap<Identifier, BootstrapContainer>,
+    ntp: NtpSettings,
+    network: NetworkSettings,
+    kernel: KernelSettings,
+    boot: BootSettings,
+    aws: AwsSettings,
+    metrics: MetricsSettings,
+    pki: HashMap<Identifier, PemCertificate>,
+    container_registry: RegistrySettings,
+    oci_defaults: OciDefaults,
+    oci_hooks: OciHooks,
+    cloudformation: CloudFormationSettings,
+    dns: DnsSettings,
+    autoscaling: AutoScalingSettings,
+}
