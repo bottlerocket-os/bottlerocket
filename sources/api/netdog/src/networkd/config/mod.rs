86c8d44b204e4fd12582b9db2ad9e24bb17ff165
Zac Mrowicki <mrowicki@amazon.com>
2023-06-19 20:17:20 +0000

    netdog: Add builder for networkd NetDevConfig
    This commit adds a buidler for the systemd-networkd NetDevConfig struct.
    The builder contains the logic to translate the structures deserialized
    from `net.toml` into NetDev structures representing systemd-networkd
    .netdev files.  Since NetDevConfig will be created for bonds and vlans
    (and potentially more later), using a builder centralizes the logic to
    avoid duplication.  Use a builder also means methods can be limited
    based on the type of device being built, providing safety at development
    time.

--- sources/api/netdog/src/networkd/config/mod.rs
+++ sources/api/netdog/src/networkd/config/mod.rs
@@ -4,7 +4,7 @@ mod netdev;
 mod network;
 
 use super::Result;
-use netdev::NetDevConfig;
+pub(crate) use netdev::{NetDevBuilder, NetDevConfig};
 use network::NetworkConfig;
 
 const NETWORKD_CONFIG_DIR: &str = "/etc/systemd/network";
@@ -19,7 +19,62 @@ impl NetworkDConfigFile {
     pub(crate) fn write_config_file(&self) -> Result<()> {
         match self {
             NetworkDConfigFile::Network(network) => network.write_config_file(),
-            NetworkDConfigFile::NetDev(netdev) => netdev.write_config_file(),
+            NetworkDConfigFile::NetDev(netdev) => netdev.write_config_file(NETWORKD_CONFIG_DIR),
         }
+    }
+}
+
+// This private module defines some empty traits meant to be used as type parameters for the
+// networkd config builders.  The type parameters limit the methods that can be called on the
+// builders so a user of this code can't inadvertently add configuration options that aren't
+// applicable to a particular device.  For example, a user can't add bond monitoring options to a
+// VLAN config.
+//
+// The following traits and enums are only meant to be used within the config module of this crate;
+// putting them in a private module guarantees this behavior.  See the "sealed trait" pattern here:
+// https://rust-lang.github.io/api-guidelines/future-proofing.html#sealed-traits-protect-against-downstream-implementations-c-sealed
+mod private {
+    // The following zero-variant enums represent the device types we currently support.  They
+    // cannot be constructed and exist only as phantom types.
+    pub enum Bond {}
+    pub enum Interface {}
+    pub enum Vlan {}
+    pub enum BondWorker {} // interfaces that are bound to a bond
+
+    // The devices for which we are generating a configuration file.  All device types should
+    // implement this trait.
+    pub trait Device {}
+    impl Device for Bond {}
+    impl Device for Interface {}
+    impl Device for Vlan {}
+    impl Device for BondWorker {}
+
+    // Devices not bound to a bond, i.e. everything EXCEPT BondWorker(s)
+    pub trait NotBonded {}
+    impl NotBonded for Bond {}
+    impl NotBonded for Interface {}
+    impl NotBonded for Vlan {}
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::networkd::devices::{NetworkDBond, NetworkDInterface, NetworkDVlan};
+    use serde::Deserialize;
+    use std::fs;
+    use std::path::{Path, PathBuf};
+
+    pub(super) const BUILDER_DATA: &str = include_str!("../../../test_data/networkd/builder.toml");
+
+    pub(super) fn test_data() -> PathBuf {
+        PathBuf::from(env!("CARGO_MANIFEST_DIR"))
+            .join("test_data")
+            .join("networkd")
+    }
+
+    #[derive(Debug, Deserialize)]
+    pub(super) struct TestDevices {
+        pub(super) interface: Vec<NetworkDInterface>,
+        pub(super) bond: Vec<NetworkDBond>,
+        pub(super) vlan: Vec<NetworkDVlan>,
     }
 }
