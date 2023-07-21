287f9d0810fc848605c3b7dcaa0a6bcdda0206a3
Matthew James Briggs <matthew.james.briggs@gmail.com>
2023-07-21 11:49:23 -0700

    build: use static files instead of /dev/null
    An unintended consequence of this `path = "/dev/null"` in the Cargo.toml
    files of packages and variants is that a change to the device causes a
    complete rebuild of all of Bottlerocket. So, for example, restarting a
    machine would cause Bottlerocket to rebuild all packages.
    
    This was an issue in the new Twoliter build system because each time
    Twoliter is executed, a container is created with a different /dev/null
    device, triggering a full rebuild.
    
    Though less cool, this commit points all packages at a single
    packages.rs file and all variants at a variants.rs file to fix this
    problem.

---
+++ variants/variants.rs
@@ -0,0 +1,7 @@
+/*!
+
+This is an intentionally empty file that all of the variant `Cargo.toml` files can point to as their
+`lib.rs`. The build system uses `build.rs` to invoke `buildsys` but Cargo needs something to compile
+so we give it an empty `lib.rs` file.
+
+!*/
