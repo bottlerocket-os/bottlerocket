1a88ea2ce3e46bcf3a7bd0be51e5d0b2e0ab0b70
Arnaldo Garcia Rincon <agarrcia@amazon.com>
2023-09-01 18:37:56 +0000

    migrations: update settings for log4j-hotpatch-enabled
    Signed-off-by: Arnaldo Garcia Rincon <agarrcia@amazon.com>

---
+++ sources/api/migration/migrations/v1.15.0/deprecate-log4j-hotpatch-enabled/src/main.rs
@@ -0,0 +1,22 @@
+use migration_helpers::common_migrations::AddPrefixesMigration;
+use migration_helpers::{migrate, Result};
+use std::process;
+
+/// We added a new service to log a warning to the journal when log4j-hotpatch-enabled
+/// is true
+fn run() -> Result<()> {
+    migrate(AddPrefixesMigration(vec![
+        "services.log4j-hotpatch-enabled",
+        "configuration-files.log4j-hotpatch-enabled-log-message",
+    ]))
+}
+
+// Returning a Result from main makes it print a Debug representation of the error, but with Snafu
+// we have nice Display representations of the error, so we wrap "main" (run) and print any error.
+// https://github.com/shepmaster/snafu/issues/110
+fn main() {
+    if let Err(e) = run() {
+        eprintln!("{}", e);
+        process::exit(1);
+    }
+}
---
+++ sources/api/migration/migrations/v1.15.0/log4j-hotpatch-enabled-metadata/src/main.rs
@@ -0,0 +1,23 @@
+use migration_helpers::common_migrations::{AddMetadataMigration, SettingMetadata};
+use migration_helpers::{migrate, Result};
+use std::process;
+
+/// We added a new service to print a warning when log4j-hotpatch-enabled is true.
+fn run() -> Result<()> {
+    migrate(AddMetadataMigration(&[SettingMetadata {
+        metadata: &["affected-services"],
+        setting: "settings.oci-hooks.log4j-hotpatch-enabled",
+    }]))?;
+
+    Ok(())
+}
+
+// Returning a Result from main makes it print a Debug representation of the error, but with Snafu
+// we have nice Display representations of the error, so we wrap "main" (run) and print any error.
+// https://github.com/shepmaster/snafu/issues/110
+fn main() {
+    if let Err(e) = run() {
+        eprintln!("{}", e);
+        process::exit(1);
+    }
+}
