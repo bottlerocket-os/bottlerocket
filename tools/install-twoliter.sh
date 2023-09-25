21a56aea1b01e136b869409c7786fca7152790d5
Matthew James Briggs <matthew.james.briggs@gmail.com>
2023-09-25 15:34:14 -0700

    build: twoliter skip version check
    Allow the skipping of the Twoliter version when deciding whether or not
    to install Twoliter. This allows us to accept any installed Twoliter
    binary and move on without re-installing.

--- tools/install-twoliter.sh
+++ tools/install-twoliter.sh
@@ -45,6 +45,10 @@ Usage: $0 -r GIT_REPO -v TWOLITER_VERSION -d INSTALL_DIR [-e REUSE_EXISTING] [-b
     -s, --allow-from-source       we will install from source using cargo install pointed to a git
                                   repo and rev when binary install is either not allowed or not
                                   possible
+    -k, --skip-version-check      do not check to see if the installed version matches the one that
+                                  is requested by the --version argument. twoliter will not be
+                                  installed when the binary is present, regardless of what version
+                                  it is.
     -h, --help                    show this help text
 
 Example invocation:
@@ -95,6 +99,8 @@ while [[ $# -gt 0 ]]; do
             allow_bin="true" ;;
         -s|--allow-from-source)
             from_source="true" ;;
+        -k|--skip-version-check)
+            skip_version_check="true" ;;
         -h|--help)
             usage; exit 0 ;;
         *)
@@ -110,13 +116,13 @@ on_exit "rm -rf ${workdir}"
 
 if [ "${reuse_existing}" = "true" ] ; then
    if [ -x "${dir}/twoliter" ] ; then
-      if [ "${allow_bin}" != "true" ]; then
-        echo "Twoliter binary found and --allow-binary-install is false. Skipping install."
+      if [ "${skip_version_check}" = "true" ]; then
+        echo "Twoliter binary found and --skip-version-check is true. Skipping install."
         exit 0
       fi
       version_output="$("${dir}/twoliter" --version)"
       found_version=v$(echo $version_output | awk '{print $2}')
-      echo "Found twoliter ${found_version} installed."
+      echo "Found Twoliter ${found_version} installed."
       if [ "${found_version}" = "${version}" ] ; then
          echo "Skipping installation."
          exit 0
@@ -131,7 +137,7 @@ if [ "${allow_bin}" = "true" ] ; then
    host_kernel="${host_kernel,,}"
    case "${host_kernel}-${host_arch}" in
       linux-x86_64 | linux-aarch64)
-      echo "Installing twoliter from binary release."
+      echo "Installing Twoliter from binary release."
       twoliter_release="${repo}/releases/download/${version}"
       twoliter_target="${host_arch}-unknown-${host_kernel}-musl"
       cd "${workdir}"
@@ -146,10 +152,11 @@ if [ "${allow_bin}" = "true" ] ; then
       ;;
    esac
 else
-   echo "Skipped installing twoliter ${version} from pre-built binaries."
+   echo "Skipping binary installation of twoliter ${version} because --allow-binary-install was not set."
 fi
 
 if [ "${from_source}" = "true" ] ; then
+   echo "Installing Twoliter version ${version} from source"
    cargo +nightly install \
      -Z bindeps \
      --locked \
