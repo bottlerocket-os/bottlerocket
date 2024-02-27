97a18d59a27e730e1e74d0f84e1a4a1426885319
Sam Berning <bernings@amazon.com>
2024-02-27 23:20:54 +0000

    early-boot-config: create dummy vmware-guestinfo provider for arm
    Signed-off-by: Sam Berning <bernings@amazon.com>

--- sources/api/early-boot-config/src/bin/user-data-providers/vmware_guestinfo.rs
+++ sources/api/early-boot-config/src/bin/user-data-providers/vmware_guestinfo.rs
@@ -1,8 +1,17 @@
+#[cfg(target_arch = "x86_64")]
 use early_boot_config::provider::{run_userdata_provider, setup_provider_logging, VmwareGuestinfo};
 use std::process::ExitCode;
 
 #[tokio::main]
 async fn main() -> ExitCode {
-    setup_provider_logging();
-    run_userdata_provider(&VmwareGuestinfo).await
+    #[cfg(target_arch = "x86_64")]
+    {
+        setup_provider_logging();
+        run_userdata_provider(&VmwareGuestinfo).await
+    }
+    #[cfg(not(target_arch = "x86_64"))]
+    {
+        println!("");
+        ExitCode::SUCCESS
+    }
 }
