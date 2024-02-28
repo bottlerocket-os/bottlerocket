08be083bf17354de5ec4014a4c2893acec386230
Sam Berning <bernings@amazon.com>
2024-02-28 01:46:35 +0000

    early-boot-config: create user-data-provider library
    Signed-off-by: Sam Berning <bernings@amazon.com>

---
+++ sources/api/user-data-provider/src/lib.rs
@@ -0,0 +1,17 @@
+/*!
+# Introduction
+
+user-data-provider defines the interface of the user data provider binaries used by early-boot-config.
+
+User data provider binaries can also be easily created using the UserDataProvider trait and logging functions defined by this crate.
+*/
+
+#[macro_use]
+extern crate log;
+
+pub mod compression;
+pub mod provider;
+pub mod settings;
+
+/// The environment variable used to set log level for env_logger
+pub const LOG_LEVEL_ENV_VAR: &str = "EARLY_BOOT_CONFIG_LOG_LEVEL";
