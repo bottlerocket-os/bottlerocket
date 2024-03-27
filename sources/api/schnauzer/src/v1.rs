d70c254f26cbe68070d3f3e1f23753a16727118c
Arnaldo Garcia Rincon <agarrcia@amazon.com>
2024-03-27 14:40:22 +0000

    schnauzer: add negate_or_else template helper
    This adds the new `negate_or_else` helper which takes a boolean value
    and applies a boolean not to it.
    
    Signed-off-by: Arnaldo Garcia Rincon <agarrcia@amazon.com>

--- sources/api/schnauzer/src/v1.rs
+++ sources/api/schnauzer/src/v1.rs
@@ -135,6 +135,7 @@ pub fn build_template_registry() -> Result<handlebars::Handlebars<'static>> {
     template_registry.register_helper("etc_hosts_entries", Box::new(helpers::etc_hosts_entries));
     template_registry.register_helper("any_enabled", Box::new(helpers::any_enabled));
     template_registry.register_helper("oci_defaults", Box::new(helpers::oci_defaults));
+    template_registry.register_helper("negate_or_else", Box::new(helpers::negate_or_else));
 
     Ok(template_registry)
 }
