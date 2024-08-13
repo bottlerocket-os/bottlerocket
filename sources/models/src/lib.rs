331b5eae03edaf3cc5ca25de4954d26cbda16bcd
Gavin Inglis <giinglis@amazon.com>
2024-08-13 19:37:19 +0000

    docs: update models README references
    update references in the models readme to their new locations.
    
    Signed-off-by: Gavin Inglis <giinglis@amazon.com>

--- sources/models/src/lib.rs
+++ sources/models/src/lib.rs
@@ -5,7 +5,7 @@ Bottlerocket has different variants supporting different features and use cases.
 Each variant has its own set of software, and therefore needs its own configuration.
 We support having an API model for each variant to support these different configurations.
 
-The model here defines a top-level `Settings` structure, and delegates the actual implementation to a ["settings plugin"](https://github.com/bottlerocket/bottlerocket-settings-sdk/tree/settings-plugins).
+The model here defines a top-level `Settings` structure, and delegates the actual implementation to a ["settings plugin"](https://github.com/bottlerocket-os/bottlerocket-settings-sdk/tree/develop/bottlerocket-settings-plugin).
 Settings plugin are written in Rust as a "cdylib" crate, and loaded at runtime.
 
 Each settings plugin must define its own private `Settings` structure.
@@ -13,7 +13,7 @@ It can use pre-defined structures inside, or custom ones as needed.
 
 `apiserver::datastore` offers serialization and deserialization modules that make it easy to map between Rust types and the data store, and thus, all inputs and outputs are type-checked.
 
-At the field level, standard Rust types can be used, or ["modeled types"](src/modeled_types) that add input validation.
+At the field level, standard Rust types can be used, or ["modeled types"](https://github.com/bottlerocket-os/bottlerocket-settings-sdk/tree/develop/bottlerocket-settings-models/modeled-types) that add input validation.
 
 The `#[model]` attribute on Settings and its sub-structs reduces duplication and adds some required metadata; see [its docs](model-derive/) for details.
 */
