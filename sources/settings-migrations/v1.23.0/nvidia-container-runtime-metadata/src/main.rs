2b094765be821f58e21a53e11ee234d83f6d06a2
Arnaldo Garcia Rincon <agarrcia@amazon.com>
2024-09-09 02:49:35 +0000

    migration: add migrations for new NVIDIA APIs
    Signed-off-by: Arnaldo Garcia Rincon <agarrcia@amazon.com>

---
+++ sources/settings-migrations/v1.23.0/nvidia-container-runtime-metadata/src/main.rs
@@ -0,0 +1,21 @@
+use migration_helpers::common_migrations::{AddMetadataMigration, SettingMetadata};
+use migration_helpers::{migrate, Result};
+use std::process;
+
+/// We added a new setting for configuring container runtime (containerd) settings only for NVIDIA k8s variants.
+fn run() -> Result<()> {
+    migrate(AddMetadataMigration(&[SettingMetadata {
+        metadata: &["affected-services"],
+        setting: "settings.nvidia-container-runtime",
+    }]))
+}
+
+// Returning a Result from main makes it print a Debug representation of the error, but with Snafu
+// we have nice Display representations of the error, so we wrap "main" (run) and print any error.
+// https://github.com/shepmaster/snafu/issues/110
+fn main() {
+    if let Err(e) = run() {
+        eprintln!("{}", e);
+        process::exit(1);
+    }
+}
